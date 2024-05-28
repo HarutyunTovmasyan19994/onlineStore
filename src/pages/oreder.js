d16a9e7ac4599e3a2d1f9a762070785f14b39817
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2024-05-28 12:13:34 +0400

    changes popoxakan

--- src/pages/oreder.js
+++ src/pages/oreder.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import {useSelector,useDispatch} from "react-redux";
 import {ArrowLeftOutlined,ShoppingOutlined} from "@ant-design/icons";
