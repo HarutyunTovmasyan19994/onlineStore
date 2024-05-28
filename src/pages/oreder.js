a466346e558e17bc7b7273fd060ea44d70eeb939
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2024-05-28 12:10:15 +0400

    changes popoxakan

--- src/pages/oreder.js
+++ src/pages/oreder.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {useSelector,useDispatch} from "react-redux";
 import {ArrowLeftOutlined,ShoppingOutlined} from "@ant-design/icons";
 import {ORDER_PRUDUCTS} from "../redux/reducers/orderReducers/action"
